0a655261e2aa6fb5a939fcafe61030d979889803
Rodrigo <rl44610@hotmail.com>
2019-08-01 02:13:56 -0400

    Siguiendo tu cosa

--- Prueba1/src/prueba1/Prueba1.java
+++ Prueba1/src/prueba1/Prueba1.java
@@ -4,6 +4,8 @@ public class Prueba1 {
 
     public static void main(String[] args) {
         //Hola que hace 
+
+        System.out.printl("Esto es lo que sigue");
     }
     //system.out.print("holaque hace")
 }
