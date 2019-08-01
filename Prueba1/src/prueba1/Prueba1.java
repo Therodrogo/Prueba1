1c5a972ef42c5ca9143753510da93f7c8740101e
thecarloito <thecarloito@gmail.com>
2019-08-01 02:35:19 -0400

    sdasda

--- Prueba1/src/prueba1/Prueba1.java
+++ Prueba1/src/prueba1/Prueba1.java
@@ -5,7 +5,7 @@ public class Prueba1 {
     public static void main(String[] args) {
         //Hola que hace 
 
-
+        int edadlegal=18;
         int edad=18;
         System.out.println("hola que hace");
         System.out.println(edad);
