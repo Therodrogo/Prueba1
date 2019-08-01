151ad48e09a6ac1e7c9f0c6a0464cbc8c45512b3
Rodrigo <rl44610@hotmail.com>
2019-08-01 02:37:17 -0400

    Crear Clases Persona con sus atributoos

---
+++ Prueba1/src/prueba1/Persona.java
@@ -0,0 +1,20 @@
+package prueba1;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Persona {
+    
+    private String Nombre;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
+    
+}
